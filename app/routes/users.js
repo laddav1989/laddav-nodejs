80870bc6ca3bdbe49e48bd309014240fd3131fad
laddav1989 <ladanyidavid1989@freemail.hu>
2023-05-17 17:50:03 +0200

    user kezelés

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db=require('../database');
 
 /* GET users listing. */
-router.get('/user-list', function(req, res, next) {
+router.get('/list', function(req, res, next) {
   db.query("SELECT * FROM users", function (err, data, fields) {
     if (err) throw err;
     res.render('user-list', { title: 'User List', userData: data});
@@ -11,10 +11,20 @@ router.get('/user-list', function(req, res, next) {
 });
 
 /* GET user datas. */
-router.get('/user/:id', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
   db.query("SELECT * FROM users WHERE id = ?", [req.params.id], function (err, data, fields) {
     if (err) throw err;
     res.render('user', { title: 'User Data', userData: data});
+  });
+});
+
+/* UPDATE user api token. */
+router.post('/new-token', function(req, res) {
+  var aD = new Date();
+  var randomString = Math.random().toString(36).slice(2, 7)+'-'+aD.getFullYear()+aD.getMonth()+aD.getDate()+aD.getHours()+aD.getMinutes()+aD.getSeconds();
+  db.query("UPDATE users SET api_token=? WHERE id = ?", [randomString, req.body.id], function (err, data, fields) {
+    if (err) throw err;
+    res.redirect('/users/'+req.body.id);
   });
 });
 
